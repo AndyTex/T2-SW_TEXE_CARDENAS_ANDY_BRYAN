eb57e81d70b51a4c12548f31f83b1445616e795d
WhiteNight07181 <andycnblue1234@gmail.com>
2024-06-23 11:14:49 -0500

    primer pregunta c1

---
+++ src/test/java/pe/edu/cibertec/exament2/Exament2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.exament2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exament2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
